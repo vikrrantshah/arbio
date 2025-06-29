94df8e095247e0f61022ecc441cfb44574e8c07c
Vikrant Shah <60457380+vikrrantshah@users.noreply.github.com>
2025-06-29 18:30:00 +0200

    Add Datasource and Prisma ORM

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DBModule } from '@arbio/db';
 
 @Module({
-  imports: [],
+  imports: [DBModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
