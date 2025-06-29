c7950c0129fc6a1dff7e1c14c705de8246a33181
Vikrant Shah <60457380+vikrrantshah@users.noreply.github.com>
2025-06-29 18:40:27 +0200

    Add swagger docs to both auth and backend

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,17 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'auth';
+
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Arbio Auth Server')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, documentFactory);
+
+  const port = process.env.PORT || 4001;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,16 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Arbio Todo Backend')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, documentFactory);
+
   const port = process.env.PORT || 4000;
   await app.listen(port);
   Logger.log(
